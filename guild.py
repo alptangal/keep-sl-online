a30aea72997b6f7aa352e81b3b5f624c49d0f06f
alptangal <alptangal@gmail.com>
2024-06-04 16:06:29 +0700

    .

--- guild.py
+++ guild.py
@@ -8,5 +8,7 @@ async def getBasic(guild):
             for channel in category.channels:
                 if 'urls' in channel.name:
                     urlsCh=channel
+                elif 'raw' in channel.name:
+                    rawCh=channel
 
-    return {'streamlitCate':streamlitCate if 'streamlitCate' in locals else None,'urlsCh':urlsCh if 'urlsCh' in locals else None}
+    return {'streamlitCate':streamlitCate if 'streamlitCate' in locals else None,'urlsCh':urlsCh if 'urlsCh' in locals else None,'rawCh':rawCh if 'rawCh' in locals else None}
--- main.py
+++ main.py
@@ -25,7 +25,7 @@ URL_STREAM='https://shoebee-fswaboivdxpaan5ewbppbf.streamlit.app/'
 
 @client.event
 async def on_ready():
-    global RESULT
+    global RESULT,GUILD_ID
     try:
         req=requests.get('http://localhost:8888')
         print(req.status_code)
@@ -43,9 +43,41 @@ async def on_ready():
         if not RESULT['streamlitCate']:
             RESULT['streamlitCate']=await guild.create_category(name='streamlit',overwrites=overwrites)
             RESULT['urlsCh']=await RESULT['streamlitCate'].create_forum(name='urls',overwrites=overwrites)
+            RESULT['rawCh']=await RESULT['streamlitCate'].create_text_channel(name='raw',overwrites=overwrites)
         if not keepLive.is_running():
             keepLive.start()
-    
+        if not updateUrl.is_running():
+            updateUrl.start()
+@tasks.loop(seconds=1)
+async def updateUrl():
+    global RESULT   
+    async for msg in RESULT['rawCh'].history():
+        if msg.content.strip() not in str(RESULT['urlsCh'].threads):
+            await RESULT['urlsCh'].create_thread(name=msg.content.strip(),content='hello world')
+            BASE_URL=msg.content.strip()
+            headers={
+                'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0'
+            }
+            async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar()) as session:
+                async with session.get(BASE_URL,headers=headers,allow_redirects=False) as res:
+                    if res.status<400:
+                        location=res.headers['location']
+                        headers['cookie']=''
+                        async with session.get(location,headers=headers,allow_redirects=False) as res:
+                            cookies = session.cookie_jar.filter_cookies('streamlit.app')
+                            for key, cookie in cookies.items():
+                                headers['cookie'] += cookie.key +'='+cookie.value+';'
+                            async with session.get(BASE_URL+'api/v2/app/disambiguate',headers=headers) as res:
+                                if res.status<400:
+                                    headers['x-csrf-token']=res.headers['x-csrf-token']
+                                    url=BASE_URL+'api/v2/app/status'
+                                    req=requests.get(url,headers=headers)
+                                    js=req.json()
+                                    if js['status']!=5:
+                                        url=BASE_URL+'api/v2/app/resume'
+                                        req=requests.post(url,headers=headers)
+                                    requests.get(BASE_URL,headers=headers)
+                                    print(BASE_URL,'Ping success!')
 @tasks.loop(minutes=15)
 async def keepLive():
     global RESULT
