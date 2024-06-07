494baab017b21fa3352bc24340c010f70e359f23
alptangal <alptangal@gmail.com>
2024-06-07 13:13:03 +0700

    .

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ async def on_ready():
     global RESULT,GUILD_ID
     try:
         req=requests.get('http://localhost:8888')
+        print(req.text)
         if int(str(datetime.datetime.now().timestamp()).split('.')[0])-int(req.text.split('.')[0])>=10:
             raise Exception("Server not response")
         await client.close() 
