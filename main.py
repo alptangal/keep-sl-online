f35601d0fe6b9e7dc17cefbe044630c644b8c6b8
alptangal <75027700+alptangal@users.noreply.github.com>
2024-10-08 23:30:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 RESULT = None
 URL_STREAM='https://shoebee-fswaboivdxpaan5ewbppbf.streamlit.app/'
-
+NEXT=False
 @client.event
 async def on_ready():
     global RESULT,GUILD_ID
@@ -36,6 +36,7 @@ async def on_ready():
         sys.exit("Exited")
     except Exception as error:
         print(error)
+        #if 'No connection could be made because the target machine actively refused it' in str(error) or "Server not response" in str(error):
         server.b()  
         guild = client.get_guild(GUILD_ID)
         RESULT=await getBasic(guild)
@@ -47,6 +48,8 @@ async def on_ready():
             RESULT['streamlitCate']=await guild.create_category(name='streamlit',overwrites=overwrites)
             RESULT['urlsCh']=await RESULT['streamlitCate'].create_forum(name='urls',overwrites=overwrites)
             RESULT['rawCh']=await RESULT['streamlitCate'].create_text_channel(name='raw',overwrites=overwrites)
+        if not restartProcess.is_running():
+            restartProcess.start()
         if not keepLive.is_running():
             keepLive.start(guild)
 @tasks.loop(seconds=15)
@@ -55,6 +58,12 @@ async def updateUrl():
     obj={}
     try:
         async for msg in RESULT['rawCh'].history():
+            '''
+            if msg.content.strip() not in obj:
+                obj[msg.content.strip()]=''
+            else:
+                await msg.delete()
+            '''
             if msg.content.strip() not in str(RESULT['urlsCh'].threads):
                 url=msg.content.strip().split(' || ')[0]
                 await RESULT['urlsCh'].create_thread(name=msg.content.strip(),content=url)
@@ -84,6 +93,14 @@ async def updateUrl():
                                                 print(BASE_URL,'Ping success!')
     except:
         pass
+@tasks.loop(hours=12)
+async def restartProcess():
+    global NEXT
+    if not NEXT:
+        NEXT=True
+    else:
+        print('Restarting process')
+        os.execv(sys.executable, [sys.executable] + sys.argv)
 @tasks.loop(seconds=30)
 async def keepLive(guild):
     global RESULT
@@ -95,45 +112,83 @@ async def keepLive(guild):
         async for msg in RESULT['rawCh'].history(oldest_first=True):
             BASE_URL=msg.content.strip().split(' || ')[0]
             print(BASE_URL+' processing')
-            isPaused=False
             async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar()) as session:
-                async with session.get(BASE_URL+'api/v2/app/status',headers=headers) as res:
+                async with session.get(BASE_URL,headers=headers,allow_redirects=False) as res:
                     if res.status<400:
-                        js=await res.json()
-                        if js['status']!=5:
-                            isPaused=True
-                if not isPaused:
-                    async with session.get(BASE_URL,headers=headers,allow_redirects=False) as res:
-                        if res.status<400:
-                            if not location:
-                                location=res.headers['location']
-                                headers['cookie']=''
-                                async with session.get(location,headers=headers,allow_redirects=False) as res:
-                                    if res.status<400:
-                                        location=res.headers['location']
-                                        async with session.get(location,headers=headers,allow_redirects=False) as res:
-                                            if res.status<400:
-                                                location=res.headers['location']
-                                                async with session.get(location,headers=headers,allow_redirects=False) as res:
-                                                    if res.status<400:
-                                                        async with session.get(location+'api/v2/app/context',headers=headers,allow_redirects=False) as res:
+                        if not location:
+                            location=res.headers['location']
+                            headers['cookie']=''
+                            async with session.get(location,headers=headers,allow_redirects=False) as res:
+                                if res.status<400:
+                                    location=res.headers['location']
+                                    async with session.get(location,headers=headers,allow_redirects=False) as res:
+                                        if res.status<400:
+                                            location=res.headers['location']
+                                            async with session.get(location,headers=headers,allow_redirects=False) as res:
+                                                if res.status<400:
+                                                    async with session.get(location+'api/v2/app/context',headers=headers,allow_redirects=False) as res:
+                                                        req=requests.get(location+'api/v2/app/context',headers=headers)
+                                                        headers['x-csrf-token']=res.headers['x-csrf-token']
+                                                        if res.status<400:
+                                                            cookies = session.cookie_jar.filter_cookies(BASE_URL)
+                                                            for key, cookie in cookies.items():
+                                                                headers['cookie'] += cookie.key +'='+cookie.value+';'
+                                                            async with session.get(BASE_URL+'api/v2/app/disambiguate',headers=headers) as res:
+                                                                if res.status<400:
+                                                                    async with session.get(BASE_URL+'api/v2/app/status',headers=headers) as res:
+                                                                        if res.status<400:
+                                                                            js=await res.json()
+                                                                            if js['status']!=5:
+                                                                                print(BASE_URL,'Resuming...')
+                                                                                url=BASE_URL+'api/v2/app/resume'
+                                                                                async with session.post(url,headers=headers) as res:
+                                                                                    if res.status<400:
+                                                                                        stop=False
+                                                                                        i=0
+                                                                                        while not stop:
+                                                                                            async with session.get(BASE_URL+'api/v2/app/status',headers=headers) as res:
+                                                                                                if res.status<400:
+                                                                                                    js=await res.json()
+                                                                                                    if js['status']==5:
+                                                                                                        stop=True
+                                                                                            if i==20:
+                                                                                                stop=True
+                                                                                            await asyncio.sleep(2)
+                                                                                            i+=1
+                                                                print(BASE_URL,'Ping success!')
+                        else:
+                            async with session.get(BASE_URL+'api/v2/app/context',headers=headers,allow_redirects=False) as res:
+                                if res.status<400:
+                                    headers['x-csrf-token']=res.headers['x-csrf-token']
+                                    cookies = session.cookie_jar.filter_cookies(BASE_URL)
+                                    for key, cookie in cookies.items():
+                                        headers['cookie'] += cookie.key +'='+cookie.value+';'
+                                    async with session.get(BASE_URL+'api/v2/app/disambiguate',headers=headers) as res:
+                                        if res.status<400:
+                                            async with session.get(BASE_URL+'api/v2/app/status',headers=headers) as res:
+                                                if res.status<400:
+                                                    js=await res.json()
+                                                    if js['status']!=5:
+                                                        print(BASE_URL,'Resuming...')
+                                                        url=BASE_URL+'api/v2/app/resume'
+                                                        async with session.post(url,headers=headers) as res:
                                                             if res.status<400:
-                                                                cookies = session.cookie_jar.filter_cookies(location)
-                                                                for key, cookie in cookies.items():
-                                                                    headers['cookie'] += cookie.key +'='+cookie.value+';'
-                                                                async with session.get(BASE_URL+'api/v2/app/disambiguate',headers=headers) as res:
-                                                                    print(BASE_URL,'Ping success!')
-                            else:
-                                async with session.get(BASE_URL+'api/v2/app/context',headers=headers,allow_redirects=False) as res:
-                                    if res.status<400:
-                                        cookies = session.cookie_jar.filter_cookies(location)
-                                        for key, cookie in cookies.items():
-                                            headers['cookie'] += cookie.key +'='+cookie.value+';'
-                                        async with session.get(BASE_URL+'api/v2/app/disambiguate',headers=headers) as res:
-                                            print(BASE_URL,'Ping success!')
+                                                                stop=False
+                                                                i=0
+                                                                while not stop:
+                                                                    async with session.get(BASE_URL+'api/v2/app/status',headers=headers) as res:
+                                                                        if res.status<400:
+                                                                            js=await res.json()
+                                                                            if js['status']==5:
+                                                                                stop=True
+                                                                    if i==20:
+                                                                        stop=True
+                                                                    await asyncio.sleep(2)
+                                                                    i+=1
+                                        print(BASE_URL,'Ping success!')
             id=int(msg.content.strip().split(' || ')[1])
             for member in guild.members:
-                if id==member.id and str(member.status)=='offline' or isPaused:
+                if id==member.id and str(member.status)=='offline':
                     async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar()) as session:
                         async with session.get(BASE_URL,headers=headers,allow_redirects=False) as res:
                             if res.status<400:
@@ -150,12 +205,12 @@ async def keepLive(guild):
                                                         if res.status<400:
                                                             async with session.get(location+'api/v2/app/context',headers=headers,allow_redirects=False) as res:
                                                                 if res.status<400:
-                                                                    cookies = session.cookie_jar.filter_cookies(location)
+                                                                    headers['x-csrf-token']=res.headers['x-csrf-token']
+                                                                    cookies = session.cookie_jar.filter_cookies(BASE_URL)
                                                                     for key, cookie in cookies.items():
                                                                         headers['cookie'] += cookie.key +'='+cookie.value+';'
                                                                     async with session.get(BASE_URL+'api/v2/app/disambiguate',headers=headers) as res:
                                                                         if res.status<400:
-                                                                            headers['x-csrf-token']=res.headers['x-csrf-token']
                                                                             url=BASE_URL+'api/v2/app/status'
                                                                             async with session.get(url,headers=headers) as res:
                                                                                 js=await res.json()
@@ -182,6 +237,9 @@ async def keepLive(guild):
                                                                                         print(res.status,2222222)
                                                                                 async with session.get(BASE_URL,headers=headers) as res:
                                                                                     print(res.status)
+                                                                                
+                                                                                '''await RESULT['urlsCh'].create_thread(name=BASE_URL,content=BASE_URL)
+                                                                                print(BASE_URL,'Ping success!')'''
                                                                         else:
                                                                             try:
                                                                                 await msg.delete()
@@ -191,12 +249,12 @@ async def keepLive(guild):
                                 else:
                                     async with session.get(location+'api/v2/app/context',headers=headers,allow_redirects=False) as res:
                                         if res.status<400:
-                                            cookies = session.cookie_jar.filter_cookies(location)
+                                            headers['x-csrf-token']=res.headers['x-csrf-token']
+                                            cookies = session.cookie_jar.filter_cookies(BASE_URL)
                                             for key, cookie in cookies.items():
                                                 headers['cookie'] += cookie.key +'='+cookie.value+';'
                                             async with session.get(BASE_URL+'api/v2/app/disambiguate',headers=headers) as res:
                                                 if res.status<400:
-                                                    headers['x-csrf-token']=res.headers['x-csrf-token']
                                                     url=BASE_URL+'api/v2/app/status'
                                                     async with session.get(url,headers=headers) as res:
                                                         js=await res.json()
@@ -243,6 +301,8 @@ async def keepLive(guild):
                                                                 if i==10:
                                                                     stop=True
                                                                 i+=1
+                                                                
+                                                            
                                                         async with session.get(location+'api/v2/app/context',headers=headers,allow_redirects=False) as res:
                                                             if res.status<400:
                                                                 cookies = session.cookie_jar.filter_cookies(location)
@@ -256,7 +316,10 @@ async def keepLive(guild):
                                                                             url=BASE_URL+'api/v2/app/status'
                                                                             async with session.get(url,headers=headers) as res:
                                                                                 print(res.status)
-                                                                                await asyncio.sleep(60)
+                                                                                await asyncio.sleep(30)
+                                                        
+                                                        '''await RESULT['urlsCh'].create_thread(name=BASE_URL,content=BASE_URL)
+                                                        print(BASE_URL,'Ping success!')'''
                                                 else:
                                                     try:
                                                         await msg.delete()
